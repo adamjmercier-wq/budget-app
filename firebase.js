1125ba055380f2f4c27f84d41001b528f8ed9c91
Adam Mercier <adamjmercier@gmail.com>
2026-03-07 12:44:01 -0500

    Move Firebase config to environment variables

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnA3qN9vAcZpyyWLtm1fCmwUj0vz4h1wI",
-  authDomain: "budget-app-48210.firebaseapp.com",
-  projectId: "budget-app-48210",
-  storageBucket: "budget-app-48210.firebasestorage.app",
-  messagingSenderId: "892980549099",
-  appId: "1:892980549099:web:be62f107b80da3fa6d35ae",
-  measurementId: "G-JGV0FDM5PH"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
